be5b125567a2ae17c3d2c4efdb939de5511a664e
w41k4z <alainricor@gmail.com>
2024-09-19 07:31:02 +0300

    Checkpoint

--- mdg_shp_trusted/region/region.py
+++ mdg_shp_trusted/region/region.py
@@ -25,23 +25,13 @@ with open('../liste-bv.csv', 'r') as csvfile:
         if i == 0:
             i+=1
             continue
-        the_region = row[0]
+        the_region = row[1]
         bv_code = row[6]
         if bv_code.startswith('1'): # Only Antananarivo
             if the_region not in regions:
                 regions.append(the_region)
                 regs[the_region] = {"id": row[5].replace(' ', '')[0:2]}
-            
-        if the_region == 'VATOVAVY':
-            continue
-        if the_region == 'FITOVINANY':
-                the_region = 'VATOVAVY FITOVINANY'
-        if the_region == 'ATSIMO-ATSINANAN':
-                the_region = 'ATSIMO ATSINANANA'
-        if the_region == 'ALAOTRA-MANGOR':
-                the_region = 'ALAOTRA MANGORO'
-        if the_region == 'ATSIMO-ANDREFAN':
-                the_region = 'ATSIMO ANDREFANA'
+
             
 # Raw region + column P_CODE and R_CODE    
 with open('Regions.csv', 'r') as csvfile:
@@ -51,9 +41,10 @@ with open('Regions.csv', 'r') as csvfile:
         if i == 0:
             i+=1
             continue
-        regs[row[3].upper()]['PROV_ID'] = row[0]
-        regs[row[3].upper()]['P_CODE'] = row[4]
-        regs[row[3].upper()]['R_CODE'] = row[5]
+        if (row[0] == '1'):
+            regs[row[3].upper()]['PROV_ID'] = row[0]
+            regs[row[3].upper()]['P_CODE'] = row[4]
+            regs[row[3].upper()]['R_CODE'] = row[5]
 
 # Cleaned Region (with spatial data)
 import geojson
@@ -82,37 +73,38 @@ with open('Regions.geojson', 'r') as f:
         #     else:
         #         arr_coordinates.append(coord[0])
         #         arr_coordinates.append(coord[1])
-        id = regs[each["properties"]["REGION"].upper()]["id"]
-        prov_id = regs[each["properties"]["REGION"].upper()]["PROV_ID"]
-        p_code = regs[each["properties"]["REGION"].upper()]["P_CODE"]
-        r_code = regs[each["properties"]["REGION"].upper()]["R_CODE"]
-        region = each["properties"]["REGION"].upper()
-        str_geojson = json.dumps(each["geometry"])
-        # type_obj = connection.gettype("MDSYS.SDO_GEOMETRY")
-        # element_info_type_obj = connection.gettype("MDSYS.SDO_ELEM_INFO_ARRAY")
-        # ordinate_type_obj = connection.gettype("MDSYS.SDO_ORDINATE_ARRAY")
-        # obj = type_obj.newobject()
-        
-        # if each["geometry"]["type"] == 'MultiPolygon':
-        #     obj.SDO_GTYPE = 2007
-        # else:
-        #     obj.SDO_GTYPE = 2003
-        # obj.SDO_SRID = 8307
-        # obj.SDO_ELEM_INFO = element_info_type_obj.newobject()
-        # obj.SDO_ELEM_INFO.extend(sd_elem_info)
-        # obj.SDO_ORDINATES = ordinate_type_obj.newobject()
-        # obj.SDO_ORDINATES.extend(arr_coordinates)
-        
-        script = "INSERT INTO imported_regions(code, id_province, p_code, r_code, nom, geojson) values(:id, :prov_id, :p_code, :r_code, :region, :geojson)"
-        cursor.execute(script, {
-            "id": str(id),
-            "prov_id": str(prov_id),
-            "p_code": p_code,
-            "r_code": r_code,
-            "region": region,
-            "geojson": str_geojson
-        })
-        print('Tafiditra \n')
+        if each["properties"]["P_CODE"].startswith('MDG1'):
+            id = regs[each["properties"]["REGION"].upper()]["id"]
+            prov_id = regs[each["properties"]["REGION"].upper()]["PROV_ID"]
+            p_code = regs[each["properties"]["REGION"].upper()]["P_CODE"]
+            r_code = regs[each["properties"]["REGION"].upper()]["R_CODE"]
+            region = each["properties"]["REGION"].upper()
+            str_geojson = json.dumps(each["geometry"])
+            # type_obj = connection.gettype("MDSYS.SDO_GEOMETRY")
+            # element_info_type_obj = connection.gettype("MDSYS.SDO_ELEM_INFO_ARRAY")
+            # ordinate_type_obj = connection.gettype("MDSYS.SDO_ORDINATE_ARRAY")
+            # obj = type_obj.newobject()
+            
+            # if each["geometry"]["type"] == 'MultiPolygon':
+            #     obj.SDO_GTYPE = 2007
+            # else:
+            #     obj.SDO_GTYPE = 2003
+            # obj.SDO_SRID = 8307
+            # obj.SDO_ELEM_INFO = element_info_type_obj.newobject()
+            # obj.SDO_ELEM_INFO.extend(sd_elem_info)
+            # obj.SDO_ORDINATES = ordinate_type_obj.newobject()
+            # obj.SDO_ORDINATES.extend(arr_coordinates)
+            
+            script = "INSERT INTO imported_regions(code, id_province, p_code, r_code, nom, geojson) values(:id, :prov_id, :p_code, :r_code, :region, :geojson)"
+            cursor.execute(script, {
+                "id": str(id),
+                "prov_id": str(prov_id),
+                "p_code": p_code,
+                "r_code": r_code,
+                "region": region,
+                "geojson": str_geojson
+            })
+            print('Tafiditra \n')
     cursor.execute("commit")
     cursor.close()
     connection.close()
