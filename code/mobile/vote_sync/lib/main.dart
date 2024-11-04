d645925443f8a98134d6389f4d3967f0002ff9c4
w41k4z <alainricor@gmail.com>
2024-11-04 18:20:41 +0300

    Checkpoint

--- code/mobile/vote_sync/lib/main.dart
+++ code/mobile/vote_sync/lib/main.dart
@@ -4,6 +4,8 @@ import 'package:get_it/get_it.dart';
 
 import 'package:loader_overlay/loader_overlay.dart';
 
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+
 import 'package:vote_sync/config/app_colors.dart';
 import 'package:vote_sync/screens/home/home_page.dart';
 import 'package:vote_sync/screens/log-in/log_in_page.dart';
@@ -12,14 +14,21 @@ import 'package:vote_sync/set_up.dart';
 import 'package:vote_sync/widgets/loading_spinner.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await SetUp.setUpApp();
+  FlutterNativeSplash.remove();
   runApp(const App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
 
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     AppInstance appInstance = GetIt.I.get<AppInstance>();
@@ -31,6 +40,7 @@ class App extends StatelessWidget {
       },
       child: MaterialApp(
         title: 'VoteSync',
+        debugShowCheckedModeBanner: false,
         theme: _appThemeData(),
         home: pageDestination,
       ),
