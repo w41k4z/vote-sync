a9d63c878fb3a876cc0b925ed5153bab4f6b801e
w41k4z <alainricor@gmail.com>
2024-11-12 10:47:44 +0300

    Checkpoint

--- code/mobile/vote_sync/lib/screens/result/widgets/result_edit_modal.dart
+++ code/mobile/vote_sync/lib/screens/result/widgets/result_edit_modal.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 import 'package:vote_sync/config/app_colors.dart';
 import 'package:vote_sync/models/candidate.dart';
+import 'package:vote_sync/services/local_storage_service.dart';
 
 class ResultEditModal extends StatefulWidget {
   final int nullVotes;
@@ -51,6 +55,8 @@ class _ResultEditModalState extends State<ResultEditModal> {
 
   @override
   Widget build(BuildContext context) {
+    LocalStorageService localStorageService =
+        GetIt.I.get<LocalStorageService>();
     return Padding(
       padding: const EdgeInsets.all(20.0),
       child: Form(
@@ -64,18 +70,34 @@ class _ResultEditModalState extends State<ResultEditModal> {
                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
               const SizedBox(height: 20),
-              _buildNumberInputField(_nullVotesController, 'Nuls'),
+              _buildNumberInputField(
+                  _nullVotesController,
+                  'Nuls',
+                  const Icon(
+                    Icons.close,
+                    color: Colors.black,
+                    size: 33,
+                  )),
               const SizedBox(height: 16),
-              _buildNumberInputField(_blankVotesController, 'Blancs'),
+              _buildNumberInputField(
+                _blankVotesController,
+                'Blancs',
+                const Icon(
+                  Icons.layers_clear_outlined,
+                  color: Colors.black,
+                  size: 33,
+                ),
+              ),
               const SizedBox(height: 15),
               //
               const Divider(),
               //
               const SizedBox(height: 15),
               for (int i = 0; i < 4; i++) ...[
-                _buildNumberInputField(
+                _buildCandidateInputField(
                   _candidatesControllers[i],
-                  widget.candidates[i].information,
+                  widget.candidates[i],
+                  localStorageService,
                 ),
                 if (i != 3) const SizedBox(height: 16),
               ],
@@ -126,7 +148,10 @@ class _ResultEditModalState extends State<ResultEditModal> {
   }
 
   Widget _buildNumberInputField(
-      TextEditingController controller, String label) {
+    TextEditingController controller,
+    String label,
+    Icon prefixIcon,
+  ) {
     return TextFormField(
       controller: controller,
       keyboardType: const TextInputType.numberWithOptions(
@@ -137,6 +162,59 @@ class _ResultEditModalState extends State<ResultEditModal> {
         labelText: label,
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(10),
+        ),
+        prefixIcon: Padding(
+          padding: const EdgeInsets.fromLTRB(10, 10, 36, 10),
+          child: prefixIcon,
+        ),
+      ),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Veillez entrer une valeur';
+        }
+        if (int.tryParse(value) == null) {
+          return 'Entrez une valeur numérique valide';
+        }
+        if (int.parse(value) < 0) {
+          return 'Entrez une valeur positive';
+        }
+        return null;
+      },
+    );
+  }
+
+  Widget _buildCandidateInputField(
+    TextEditingController controller,
+    Candidate candidate,
+    LocalStorageService localStorageService,
+  ) {
+    return TextFormField(
+      controller: controller,
+      keyboardType: const TextInputType.numberWithOptions(
+        decimal: false,
+        signed: false,
+      ),
+      decoration: InputDecoration(
+        labelText: candidate.information,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        prefixIcon: Padding(
+          padding: const EdgeInsets.fromLTRB(10, 10, 20, 10),
+          child: Container(
+            height: 50,
+            width: 50,
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.all(Radius.circular(15.0)),
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: FileImage(
+                  File(
+                      "${localStorageService.appDocDir.path}/${candidate.imagePath}"),
+                ),
+              ),
+            ),
+          ),
         ),
       ),
       validator: (value) {
